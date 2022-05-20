7bfcdbfb59aba16c1d7fca828408241f5bdac03e
dmwema <danielmwemakapwe@gmail.com>
2022-05-20 04:02:14 +0100

    update responsive

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -16,7 +16,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class DashboardController extends AbstractDashboardController
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/azazee", name="admin")
      */
     public function index(): Response
     {
