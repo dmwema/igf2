47385a70c717ba2801a69b2623c9ef0dde4465ba
dmwema <danielmwemakapwe@gmail.com>
2022-05-20 01:45:24 +0100

    fix css not loading

---
+++ public/js/stiky.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+  var derniere_position_de_scroll_connue = 0;
+  var ticking = false;
+  let nav = document.querySelector("nav.navbar.main-nav");
+  if (window.scrollY > 100) {
+    nav.classList.add("fixed");
+    console.log("fixed");
+  } else {
+    nav.classList.remove("fixed");
+  }
+  window.addEventListener("scroll", function (e) {
+    derniere_position_de_scroll_connue = window.scrollY;
+    if (!ticking) {
+      window.requestAnimationFrame(function () {
+        if (derniere_position_de_scroll_connue > 150) {
+          nav.classList.add("fixed");
+        } else {
+          nav.classList.remove("fixed");
+        }
+        ticking = false;
+      });
+    }
+    ticking = true;
+  });
+});
